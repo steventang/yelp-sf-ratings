803f57cc65159a5132d66084ba489929722ce6fb
Steven Tang <stang12290@gmail.com>
2015-08-04 17:08:08 -0700

    Polish map, correct mapping calculations

--- yelp.rb
+++ yelp.rb
@@ -9,12 +9,8 @@ require 'csv'
 
 @params = { term: 'restaurant',
 					 limit: 10,
+					 category_filter: 'restaurants',
 					 sort: 0 }
-
-location = { location: '94109' }
-
-biz = []
-sum_ratings = 0
 
 def make_box(swlo, swla, nelo, nela) # order rearranged to fit yelp order
 	{ sw_latitude: swla, sw_longitude: swlo, ne_latitude: nela, ne_longitude: nelo }
@@ -38,14 +34,16 @@ end
 def calculate_box_rating(box)
 	box_total = 0
 	biz = bounding_box_search box
+	# puts "In bounding box #{box} #{'*' * 30}"
+	# output biz
 	biz.each { |b| box_total += b.rating }
-	avg_rating = if biz.length > 4 then box_total/biz.length else 0 end # require sample size of 5 to get a rating
+	avg_rating = if biz.length > 1 then box_total/biz.length else 0 end # require sample size of 5 to get a rating
 	avg_rating.round(2)
 end
 
 # output some stuff about the response
-def output(biz)
-	biz.each do |b|
+def output(biz_array)
+	biz_array.each do |b|
 		puts "#{b.name}, #{b.rating}, #{b.location.postal_code}"
 	end
 end
@@ -60,6 +58,6 @@ def generate_csv(box_array)
 	puts "CSV generated"
 end
 
-sample_box = make_box(-122.518631,37.70814,-122.363964,37.811471)
-box_array = split_into_boxes(sample_box, 0.02, 0.01)
+sf_box = make_box(-122.514941,37.70814,-122.35714,37.811675)
+box_array = split_into_boxes(sf_box, 0.006, 0.004)
 generate_csv(box_array)
